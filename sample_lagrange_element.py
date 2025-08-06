c19ee364197ac740a5714c359b782ada59736fd0
Lars Gebraad <lars.gebraad@mondaic.com>
2025-08-06 21:59:14 +0200

    Change test file to also be Jupytext executable

--- sample_lagrange_element.py
+++ sample_lagrange_element.py
@@ -1,60 +1,109 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python3
+"""
+Generate a sample Lagrange element file to check on vertex ordering. Change
+the code on the indicated lines for desired element type and order or specify
+them as arguments.
 
-# Written by Jens Ulrich Kreber <ju.kreber@gmail.com>
-# for the Chair for Electromagnetic Theory at Saarland University (https://www.uni-saarland.de/lehrstuhl/dyczij-edlinger.html)
+Written by Jens Ulrich Kreber <ju.kreber@gmail.com> for the Chair for
+Electromagnetic Theory at Saarland University
+(https://www.uni-saarland.de/lehrstuhl/dyczij-edlinger.html)
+"""
 
-# pylint: disable=line-too-long, no-member, unsubscriptable-object
-"""Generate a sample Lagrange element file to check on vertex ordering. Change the code on the indicated lines for desired element type and order or specify them as arguments"""
-
-
-import warnings
-import sys
+# %%
 import numpy as np
 import vtk
-import vtk.util.numpy_support as vnp
+import vtk.util.numpy_support as vnp  # type: ignore
+import matplotlib.pyplot as plt
 from node_ordering import node_ordering
 
-VTK_CELLTYPES = {'triangle': vtk.VTK_LAGRANGE_TRIANGLE, 'tetrahedron': vtk.VTK_LAGRANGE_TETRAHEDRON, 'quadrilateral': vtk.VTK_LAGRANGE_QUADRILATERAL, 'hexahedron': vtk.VTK_LAGRANGE_HEXAHEDRON, 'wedge': vtk.VTK_LAGRANGE_WEDGE}
+# %%
+VTK_CELLTYPES = {
+    "triangle": vtk.VTK_LAGRANGE_TRIANGLE,
+    "tetrahedron": vtk.VTK_LAGRANGE_TETRAHEDRON,
+    "quadrilateral": vtk.VTK_LAGRANGE_QUADRILATERAL,
+    "hexahedron": vtk.VTK_LAGRANGE_HEXAHEDRON,
+    "wedge": vtk.VTK_LAGRANGE_WEDGE,
+}
 
-if __name__ == "__main__":
-    warnings.simplefilter(action='ignore', category=FutureWarning) # TODO: check for fixed version of vtk
+# %%
+# Change to desired element here
+element_type = "hexahedron"
+element_order = 2
 
-### change to desired element here
-    element_type = 'wedge'              # <------------------------ one of 'triangle', 'quadrilateral', 'tetrahedron', 'hexahedron', 'wedge'
-    element_order = 5                   # <------------------------ 1 to 10
-###
+assert (element_type, element_order) in VTK_CELLTYPES.items()
+assert element_order in range(1, 11), "Element order must be between 1 and 10"
 
-    if len(sys.argv) == 3 and sys.argv[1] in VTK_CELLTYPES.keys() and int(sys.argv[2]) in range(1,11):
-        element_type = sys.argv[1]
-        element_order = int(sys.argv[2])
-    elif len(sys.argv) != 1:
-        sys.exit("Usage: " + sys.argv[0] + " [ELEMENT_TYPE ELEMENT_ORDER]")
+# %%
+# Create points for the element
+points = node_ordering(element_type, element_order)
 
-    points = node_ordering(element_type, element_order)
-    points_vtk_data = vnp.numpy_to_vtk(points)
-    points_vtk = vtk.vtkPoints()
-    points_vtk.SetData(points_vtk_data)
+# %%
+points_vtk_data = vnp.numpy_to_vtk(points)
+points_vtk = vtk.vtkPoints()
+points_vtk.SetData(points_vtk_data)
 
-    num_points = points.shape[0]
-    cell_data = np.concatenate((np.array([num_points]), np.arange(num_points)), axis=0)
-    id_type_np = vnp.ID_TYPE_CODE # the numpy dtype for vtkIdTypeArray
-    cell_data_vtk = vnp.numpy_to_vtk(cell_data, array_type=vtk.VTK_ID_TYPE)
-    cells = vtk.vtkCellArray()
-    cells.SetCells(1, cell_data_vtk)
+num_points = points.shape[0]
+cell_data = np.concatenate(
+    (np.array([num_points]), np.arange(num_points)), axis=0
+)
+id_type_np = vnp.ID_TYPE_CODE  # the numpy dtype for vtkIdTypeArray
+cell_data_vtk = vnp.numpy_to_vtk(cell_data, array_type=vtk.VTK_ID_TYPE)
+cells = vtk.vtkCellArray()
+cells.SetCells(1, cell_data_vtk)
 
-    pointdata = np.arange(num_points, dtype=np.float64) # simply use point index as data
-    pointdata_vtk = vnp.numpy_to_vtk(pointdata)
-    pointdata_vtk.SetName("point_numbers")
+# Simply use point index as data (can be visualized as a field in ParaView)
+pointdata = np.arange(num_points, dtype=np.float64)
+pointdata_vtk = vnp.numpy_to_vtk(pointdata)
+pointdata_vtk.SetName("point_numbers")
 
-    ugrid = vtk.vtkUnstructuredGrid()
-    ugrid.SetPoints(points_vtk)
-    ugrid.SetCells(VTK_CELLTYPES[element_type], cells)
-    pointdata_container = ugrid.GetPointData()
-    pointdata_container.SetScalars(pointdata_vtk)
+ugrid = vtk.vtkUnstructuredGrid()
+ugrid.SetPoints(points_vtk)
+ugrid.SetCells(VTK_CELLTYPES[element_type], cells)
+pointdata_container = ugrid.GetPointData()
+pointdata_container.SetScalars(pointdata_vtk)
 
-    writer = vtk.vtkXMLUnstructuredGridWriter()
-    writer.SetInputDataObject(ugrid)
-    writer.SetDataModeToAscii()
-    writer.SetCompressorTypeToNone()
-    writer.SetFileName('lagrange_sample.vtu')
-    writer.Write()
+writer = vtk.vtkXMLUnstructuredGridWriter()
+writer.SetInputDataObject(ugrid)
+writer.SetDataModeToAscii()
+writer.SetCompressorTypeToNone()
+writer.SetFileName("lagrange_sample.vtu")
+writer.Write()
+
+# %%
+# Create 3D scatter plot of node ordering
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection="3d")
+
+# Create scatter plot
+scatter = ax.scatter(
+    points[:, 0],
+    points[:, 1],
+    points[:, 2],
+    c=pointdata,
+    cmap="rainbow",
+    s=100,
+    alpha=1.0,
+)
+
+# Add node numbers as text labels
+for i, (x, y, z) in enumerate(points):
+    ax.text(x, y, z, f"  {i}", fontsize=8)
+
+# Set labels and title
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+ax.set_title(
+    f"{element_type.capitalize()} Element (Order "
+    f"{element_order}) - Node Ordering"
+)
+
+# Add colorbar
+plt.colorbar(scatter, ax=ax, shrink=0.5, aspect=5, label="Node Index")
+
+# Look down in positive direction for x/y.
+ax.view_init(elev=45, azim=-105)
+
+# Show the plot
+plt.tight_layout()
+plt.show()
